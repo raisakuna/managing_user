0a933af81ea1fcc313c2fca996c9cb481fa0f151
raisakuna <sakunaxl@gmail.com>
2024-11-07 20:51:47 -0500

    Cards and Contact model added

--- src/main/java/com/eazybytes/model/AccountTransactions.java
+++ src/main/java/com/eazybytes/model/AccountTransactions.java
@@ -2,6 +2,8 @@ package com.eazybytes.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
